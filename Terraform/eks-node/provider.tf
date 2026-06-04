eda20d215e707b59d1025600a7c4827e55f1a3d5
pranav0015 <pranavwakte580@gmail.com>
2026-06-04 17:21:05 +0530

    added variable.tf in jenksinfiel of EKS

--- Terraform/eks-node/provider.tf
+++ Terraform/eks-node/provider.tf
@@ -6,6 +6,9 @@ locals {
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+  access_key      = var.access_key
+  secret_key      = var.secret_key
+
   tags = {
     Example = local.name
   }
